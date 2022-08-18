92daa1674175b139f91e423bc371a56d0f708faf
HanYongYan <54656161@qq.com>
2022-08-18 23:27:19 +0800

    修改

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+       String num= "123";
+       int ans = Integer.parseInt(num);
+       System.out.println(ans);
+    }
+}
--- src/平常练习/已完成/_415字符串相加.java
+++ src/平常练习/已完成/_415字符串相加.java
@@ -1,4 +1,4 @@
-package 平常练习;
+package 平常练习.已完成;
 
 public class _415字符串相加 {
     public static void main(String[] args) {
