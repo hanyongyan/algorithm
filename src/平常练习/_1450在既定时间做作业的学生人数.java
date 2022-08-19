a3d8282c13df81575216fdbbce19d6a6fc6b251a
HanYongYan <54656161@qq.com>
2022-08-19 10:36:36 +0800

    _1450在既定时间做作业的学生人数.java

---
+++ src/平常练习/_1450在既定时间做作业的学生人数.java
@@ -0,0 +1,19 @@
+package 平常练习;
+
+public class _1450在既定时间做作业的学生人数 {
+    public static void main(String[] args) {
+        System.out.println(busyStudent(new int[] { 3 }, new int[] { 4 }, 4));
+    }
+
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int ans = 0;
+        int len = startTime.length;
+        for (int i = 0; i < len; i++) {
+            // 开始的时间必须小于指定查询时间，结束时间必须大于指定查询时间
+            if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
