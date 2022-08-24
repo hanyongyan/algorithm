d70720c0cdf9ddf4cea71cffd43fce4cafdfae70
HanYongYan <54656161@qq.com>
2022-08-24 22:41:08 +0800

    .

--- src/test.java
+++ src/test.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
+import org.junit.Test;
+
 public class test {
     public static void main(String[] args) {
        String num= "123";
        int ans = Integer.parseInt(num);
        System.out.println(ans);
+    }
+
+    @Test
+    public void test(){
+        int i = 0;
+        i=++i;
+        System.out.println(i);
     }
 }
