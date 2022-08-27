03cf6061526cd207b0e74434bf7349b84631c893
HanYongYan <54656161@qq.com>
2022-08-28 00:12:49 +0800

    287寻找重复数

---
+++ src/平常练习/已完成/_287寻找重复数.java
@@ -0,0 +1,29 @@
+package 平常练习.已完成;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _287寻找重复数 {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+     * 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
+     * 
+     * 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+     * 
+     * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+     */
+    public static int findDuplicate(int[] nums) {
+        int len = nums.length;
+        Set<Integer> set =new HashSet<>();
+        for (int i = 0; i < len; i++) {
+            if(set.contains(nums[i])){
+                return nums[i];
+            }
+            set.add(nums[i]);
+        }
+        return -1;
+    }
+}
