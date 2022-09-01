6e3cf919a54406d65d0461ae38e4f76fb8c7e6e1
HanYongYan <54656161@qq.com>
2022-09-01 11:50:00 +0800

    518零钱兑换

--- src/平常练习/已完成/_518零钱兑换2.java
+++ src/平常练习/已完成/_518零钱兑换2.java
@@ -17,13 +17,11 @@ public class _518零钱兑换2 {
      * 题目数据保证结果符合 32 位带符号整数。
      */
     public static int change(int amount, int[] coins) {
-
+//当固定元素的时候，遍历背包的时候，dp 记录的是当前元素及以前的排列，不包含后面元素的排列，
+        //例如在固定元素为 1 时，遍历结束背包容量的时候，dp 记录的是只有元素 1 的时候，
+        //背包从空到满的排列方式。当元素为 2 的时候，dp[i-v] 如果成立， 实际上是先排列了 1 元素， 然后再排列了2。
         int[] dp = new int[amount + 1];
         dp[0] = 1;
-        // 第一次循环只使用第一个硬币 查看能否组成 i 金额
-        // 第二次循环使用第一个和第二个硬币 查看是否能够组成 i 金额
-        // ...
-        // 如看不懂 debug 一次
         for (int coin : coins) {
             for (int i = coin; i <= amount; i++) {
                 dp[i] += dp[i - coin];
@@ -31,17 +29,22 @@ public class _518零钱兑换2 {
         }
         return dp[amount];
 
+        //上面是组合
+        //下面是排列
+
+
+        // 下面这种方法会有重复计算
         // int[] dp = new int[amount + 1];
         // dp[0] = 1;
         // // currAmount代表当前的金额，
         // for (int currAmount = 1; currAmount <= amount; currAmount++) {
-        // for (int coin : coins) {
-        // // 说明从 leftAmount 金额选择 coin 就能够到达 currAmount 金额
-        // int leftAmount = currAmount - coin;
-        // if (leftAmount < 0 || leftAmount == Integer.MAX_VALUE)
-        // continue;
-        // dp[currAmount] += dp[leftAmount];
-        // }
+        //     for (int coin : coins) {
+        //         // 说明从 leftAmount 金额选择 coin 就能够到达 currAmount 金额
+        //         int leftAmount = currAmount - coin;
+        //         if (leftAmount < 0 || leftAmount == Integer.MAX_VALUE)
+        //             continue;
+        //         dp[currAmount] += dp[leftAmount];
+        //     }
         // }
         // return dp[amount];
     }
