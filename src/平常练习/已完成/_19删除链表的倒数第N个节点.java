1aa2f9c43e595644015ac1e7e263a03afa9cd78b
韩永琰 <hanyongyan8@gmail.com>
2022-12-14 15:29:00 +0800

    _19删除链表的倒数第N个节点

---
+++ src/平常练习/已完成/_19删除链表的倒数第N个节点.java
@@ -0,0 +1,25 @@
+package 平常练习.已完成;
+
+public class _19删除链表的倒数第N个节点 {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
