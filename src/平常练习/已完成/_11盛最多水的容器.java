2d398456669efa1d021f16688abc81f03012f61b
韩永琰 <hanyongyan8@gmail.com>
2022-12-22 12:03:08 +0800

    盛最多水的容器

---
+++ src/平常练习/已完成/_11盛最多水的容器.java
@@ -0,0 +1,17 @@
+package 平常练习.已完成;
+
+public class _11盛最多水的容器 {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
+    }
+
+    public static int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+        while(i < j) {
+            res = height[i] < height[j] ? 
+                Math.max(res, (j - i) * height[i++]): 
+                Math.max(res, (j - i) * height[j--]); 
+        }
+        return res;
+    }
+}
